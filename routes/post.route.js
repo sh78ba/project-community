c399a1e3419e52b1260bfe2d3cab924ad2212dfd
sh78ba <sh78ba@gmail.com>
2024-06-28 23:43:58 +0530

    posts

--- routes/post.route.js
+++
@@ -1,8 +0,0 @@
-const postMw=require("../middlewares/post.mw")
-const postController=require("../controllers/post.controller")
-
-module.exports=(app)=>{
-    app.post("/community/api/v1/post/create",[postMw.verifyPostBody],postController.createNeighbourPost);
-    app.get("/community/api/v1/post/getallposts",postController.getAllPosts)
-}
-
